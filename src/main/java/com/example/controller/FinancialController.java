ff684b06496dc8bdf74bc4ecda3c4b966836d55d
Rana Badran <ranaz@ranas-mbp.home>
2024-10-14 18:05:12 -0400

    controler fix

--- src/main/java/com/example/controller/FinancialController.java
+++ src/main/java/com/example/controller/FinancialController.java
@@ -6,11 +6,19 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.BufferedReader;
+
+ import java.nio.file.Files;
+ import java.nio.file.Path;
+
+ import java.util.stream.Collectors;
+
 
 @RestController
 public class FinancialController {
@@ -20,8 +28,10 @@ public class FinancialController {
 
     @GetMapping(value = "/balances", produces = MediaType.APPLICATION_JSON_VALUE)
     public String getBalances() throws IOException {
-        Resource resource = resourceLoader.getResource("classpath:/balances.json");
-        Path path = resource.getFile().toPath();
-        return new String(Files.readAllBytes(path));
+        
+        InputStream resource = new ClassPathResource("balances.json").getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(resource));
+        String balances = reader.lines().collect(Collectors.joining("\n"));
+        return new String(balances);
     }
 }
